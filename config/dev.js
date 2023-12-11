0c0ffb2275f1ff7ab0d0baac1c977e2f2abe92a2
Robert Lovin <robert.a.lovin@gmail.com>
2023-12-11 11:49:19 -0700

    adding to gitignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    cookieKey: 'wefwefw3423d24f34fd23dw',
-    mongoURI: "mongodb+srv://robertalovin:erBsA3o2mqCYkoL0@cluster0.hqkb3go.mongodb.net/?retryWrites=true&w=majority",
-    clientID: "893839111043-si3338tv5bggb9mlva7u4r94n8c96195.apps.googleusercontent.com",
-    clientSecret: "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"
-}
